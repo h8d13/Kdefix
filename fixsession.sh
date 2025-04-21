e38f816116eae0aae7e392499d02ce5e873a82b9
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-22 01:04:39 +0200

    # gg

--- fixsession.sh
+++ fixsession.sh
@@ -1,4 +1,7 @@
-cat <<EOF > "$CONFIG_FILE"
-[General]
-loginMode=emptySession
-EOF
+#!/bin/sh
+# prepend to file :)
+CONFIG_FILE="/home/harch/.config/ksmserverrc"
+TMP_FILE="$(mktemp)"
+echo -e "[General]\nloginMode=emptySession" > "$TMP_FILE"
+cat "$CONFIG_FILE" >> "$TMP_FILE"
+mv "$TMP_FILE" "$CONFIG_FILE"
