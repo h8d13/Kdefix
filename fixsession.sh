530f1b8f0232bc7ad30f88114e2cdc83fc397d68
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-22 01:20:51 +0200

    Update fixsession.sh

--- fixsession.sh
+++ fixsession.sh
@@ -6,13 +6,20 @@ echo -e "[General]\nloginMode=emptySession" > "$TMP_FILE"
 cat "$CONFIG_FILE" >> "$TMP_FILE"
 mv "$TMP_FILE" "$CONFIG_FILE"
 
+
 f="/home/harch/.config/kscreenlockerrc"
 t=$(mktemp)
 
 while IFS= read -r line; do
+  [[ "$line" == "[Daemon]" ]] && {
+    echo "$line" >> "$t"
+    echo "LockGrace=300" >> "$t"
+    echo "Timeout=15" >> "$t"
+    skip=1
+    continue
+  }
+  [[ "$line" == Timeout=* && $skip == 1 ]] && continue
   echo "$line" >> "$t"
-  [[ "$line" == "[Daemon]" ]] && echo "LockGrace=300" >> "$t"
-  [[ "$line" == "Timeout="* ]] && echo "Timeout=15" >> "$t" && continue
 done < "$f"
 
 mv "$t" "$f"
