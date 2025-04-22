a944aade651528495dfc11cf178d2522b63df819
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-22 08:46:35 +0200

    # ggnore

--- fixsession.sh
+++ fixsession.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-# prepend to file :)
+# cool prepend sequence to file :)
 CONFIG_FILE="/home/harch/.config/ksmserverrc"
 TMP_FILE="$(mktemp)"
 echo -e "[General]\nloginMode=emptySession" > "$TMP_FILE"
 cat "$CONFIG_FILE" >> "$TMP_FILE"
 mv "$TMP_FILE" "$CONFIG_FILE"
+# Basiclally just makes it so that new sessions are fresh. 
 
-
+# Simple override the whole file for 15 min lockout and 5 min password grace. 
 CONFIG_FILE2="/home/harch/.config/kscreenlockerrc"
 cat <<EOF > $CONFIG_FILE2
 [Daemon]
